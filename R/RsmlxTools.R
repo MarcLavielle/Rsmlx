b4d0f8c2d180969136b67d7d789df293be741c57
Marc Lavielle <Marc.Lavielle@inria.fr>
2019-04-27 10:42:23 +0200

    remove apiManager.R

--- R/RsmlxTools.R
+++ R/RsmlxTools.R
@@ -1,4 +1,37 @@
 
+#' Initialize Rsmlx library
+#' 
+#' Initialize Rsmlx library
+#' @return A boolean equaling TRUE if the initialization has been successful and FALSE if not.
+#' @examples
+#' \dontrun{
+#' initRsmlx()
+#' }
+#' @export
+initRsmlx <- function(){
+  packinfo <- utils::installed.packages()
+  if (!is.element("lixoftConnectors", packinfo[,1]))
+    stop("You need to install the lixoftConnectors package in order to use Rsmlx", call. = FALSE)
+  
+  
+  lixoftConnectorsState <- getLixoftConnectorsState(quietly = TRUE)
+  
+  if (!is.null(lixoftConnectorsState)){
+    
+    if (lixoftConnectorsState$software == "monolix") {
+      status=TRUE
+    } else {
+      status = initializeLixoftConnectors(software = "monolix")
+    }
+    
+  } else {
+    status = initializeLixoftConnectors(software = "monolix")
+  }
+  return(invisible(status))
+  
+}
+
+
 #-------------------------------------------------
 generatePKmodel <-  function(parameter, model="pk_model.txt", output=NULL) {
   str1 <- paste(parameter,collapse=",")
